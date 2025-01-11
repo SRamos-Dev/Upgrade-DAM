7a383816d9ace96b33de2939fddc11c00792859c
Sergio E. Ramos <virtualsoft.adm@gmail.com>
2025-01-11 15:46:38 +0100

    Create TestFile
    Test file to upload to GitHub

---
+++ Entrega1.java
@@ -0,0 +1,7 @@
+public class Entrega1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /*Este es un comentario de prueba */
+        /*Publicacion de prueba */
+    }
+}
