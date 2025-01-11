1318ee07c3afdc864ca13181d2fbabed038748f1
Sergio E. Ramos <virtualsoft.adm@gmail.com>
2025-01-11 16:17:33 +0100

    Creacion de proyecto Funciones
    Se elimina la prueba anterior y se crea un proyecto para practica de funciones

--- Entrega1.java
+++
@@ -1,7 +0,0 @@
-public class Entrega1 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        /*Este es un comentario de prueba */
-        /*Publicacion de prueba */
-    }
-}
---
+++ ProyectosUpgrade/src/SegundoTrimestre/EjemploFunciones.java
@@ -0,0 +1,10 @@
+package SegundoTrimestre;
+
+public class EjemploFunciones {
+	public static void main(String[] args) {
+		// Voy a crear una funcion que me permita elevar 'a' a la potencia 'b'
+		double resultado = potencia(2.8, 3.3);
+		System.out.println("El resultado de la 2.8 a 3.3 es: " + resultado);
+		
+	}
+}
