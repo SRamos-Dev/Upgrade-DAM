486a987024f1b339c1436c9ab4b43c0061b2b8f2
Sergio E. Ramos <virtualsoft.adm@gmail.com>
2025-01-11 16:59:11 +0100

    Create EjemploFunciones2.java
    Actualizacion

---
+++ ProyectosUpgrade/src/SegundoTrimestre/EjemploFunciones2.java
@@ -0,0 +1,25 @@
+package SegundoTrimestre;
+
+public class EjemploFunciones2 {
+public static void main(String[] args) {
+    // luego hago esto
+    System.out.println(esPrimo(198));
+}
+
+public static boolean esPrimo(double n){
+    // Vamos a suponer que el número es válido, es decir, es un entero positivo mayor que 1.
+    if(n==2){
+        return true;
+    }
+    if(n%2==0){
+        return false;
+    }
+    for(int i=3; i<=Math.sqrt(n); i+=2){
+        if (n%i==0){
+            System.out.println(i);
+            return false;
+        }
+    }
+    return true;
+}
+}
